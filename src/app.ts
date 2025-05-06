74d862d7f79ef02261b121586247b9892cb9c5c3
Your NameRebakum <anamikaeasmin@hotmail.com>
2025-05-07 01:51:34 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,7 @@ app.use(express.json());
 // cors origin permission
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://bike-store-b4-a4-frontend.vercel.app",
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
